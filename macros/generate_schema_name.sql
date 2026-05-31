f0b34344f6ce929a020056d5b5e73933041a73f0
mohammed ahmed <mohammedalz991@gmail.com>
2026-05-31 09:37:34 +0000

    feat: add custom schema macro and schema configs for prod deployment

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,11 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if target.name == 'prod' -%}
-        {%- if custom_schema_name is none -%}
-            {{ target.schema }}
-        {%- else -%}
-            {{ custom_schema_name | trim }}
-        {%- endif -%}
+
+    {%- if target.name == 'prod' and custom_schema_name is not none -%}
+        {{ target.schema }}_{{ custom_schema_name | trim }}
     {%- else -%}
         {{ target.schema }}
     {%- endif -%}
+
 {%- endmacro %}
