a134b9809fc0ab09b93da2928a8992bc9c6cbcf6
mohammed ahmed <mohammedalz991@gmail.com>
2026-05-31 10:39:23 +0000

    fix

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,6 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- if target.name == 'prod' and custom_schema_name is not none -%}
+    {%- if target.name == 'PROD' and custom_schema_name is not none -%}
         {{ target.schema }}_{{ custom_schema_name | trim }}
     {%- else -%}
         {{ target.schema }}
